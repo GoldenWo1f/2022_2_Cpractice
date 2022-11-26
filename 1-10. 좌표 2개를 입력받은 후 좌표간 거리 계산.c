a41105f1eef1ef3426c65886e12b11d7289a1376
GoldenWo1f <118251694+GoldenWo1f@users.noreply.github.com>
2022-11-26 20:42:53 +0900

    Add files via upload

---
+++ 1-10. 좌표 2개를 입력받은 후 좌표간 거리 계산.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float x1, x2;   // x좌표 값
+	float y1, y2;   // y좌표 값
+	float distance;  // 거리 값
+  printf("첫번째 좌표의 x값? ");
+  scanf("%f", &x1);
+  printf("첫번째 좌표의 y값? ");
+  scanf("%f", &y1);
+  printf("두번째 좌표의 x값? ");
+  scanf("%f", &x2);
+  printf("두번째 좌표의 y값? ");
+  scanf("%f", &y2);
+  distance = sqrt(pow((x2-x1),2.0)+pow((y2-y1),2.0));
+  printf("두 좌표 사이의 거리는 %.1f", distance);
+
+	return 0;
+}
